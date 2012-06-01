2728800a0082c6187bd85186fd5052478b2c9061
Chris Lucas <chris@chrisjlucas.com>
2012-05-31 21:00:52 -0700

    increased number of audio queue buffers
    It was initially set to 16 when forked. For some reason i decreased it to 3, which caused buffering issues with m4a files

--- Classes/AudioStreamer.h
+++ Classes/AudioStreamer.h
@@ -27,7 +27,7 @@
 
 #define LOG_QUEUED_BUFFERS 0
 
-#define kNumAQBufs 3			// Number of audio queue buffers we allocate.
+#define kNumAQBufs 16			// Number of audio queue buffers we allocate.
 								// Needs to be big enough to keep audio pipeline
 								// busy (non-zero number of queued buffers) but
 								// not so big that audio takes too long to begin
